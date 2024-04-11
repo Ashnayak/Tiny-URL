e4c702f9ddbaa026a2e7a11881a17c8b3f21cef5
Ashwini Nayak <ashwininayak1212@gmail.com>
2024-04-10 22:25:04 -0700

    Cleaning up

--- tests/url.test.js
+++ tests/url.test.js
@@ -3,7 +3,7 @@ const { hashLongUrl } = require('../app');
 describe('hashLongUrl', () => {
   it('generates a consistent 6-character hash for a given URL', async () => {
     const url = 'https://example.com';
-    const hash = await hashLongUrl(url); // Assuming hashLongUrl returns a Promise
+    const hash = await hashLongUrl(url); 
     expect(hash).toHaveLength(6);
   });
 });
